0262112fecd84e57564d55729874ed8f72b3b7a8
David Bellingroth <spam.dbellingroth@gmail.com>
2011-12-21 15:39:17 +0100

    includes korrigiert

--- src/dep.h
+++ src/dep.h
@@ -1,16 +1,16 @@
 #if (defined(__MACH__))
 
-#include "SDL/SDL.h"
-#include "SDL/SDL_opengl.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
 
 #elif defined (__linux__)
 
-#include "SDL/SDL.h"
-#include "SDL/SDL_opengl.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
 
 #elif defined (_WIN32)
 
-#include "SDL.h"
-#include "SDL_opengl.h"
+#include <SDL.h>
+#include <SDL_opengl.h>
 
 #endif
